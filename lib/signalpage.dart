58bd0d05f1eb76eb6d7e4b3b79230b2b11eb3805
Martin Maråk <martin.maraak@ulstein.com>
2018-12-16 13:58:02 +0100

    Added a graph

--- lib/signalpage.dart
+++ lib/signalpage.dart
@@ -19,18 +19,55 @@ class SignalPage extends StatelessWidget{
         body: Center(
           child:
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
             children: <Widget>[
+              Text("Description: " +_signal.description),
               Text("Tag: " +_signal.tag),
               Text("Type: " +_signal.type),
               Text("Interface: " +_signal.interface),
               Text("Address: " +_signal.address),
               Text("Unit: " +_signal.unit),
+              Container(
+                margin: const EdgeInsets.all(4.0),
+                height: 200,
+                width: 400,
+                child:
+                TimeSeriesChart(_getSignalTimeSeriesData()),
+              ),
             ],
           )
         ),
       );
+  }
+
+  List<Series<TimeSeriesSignal,DateTime>>_getSignalTimeSeriesData(){
+
+    final exampleData = [
+    new TimeSeriesSignal(new DateTime(2018,12,24,16,00,01), 10.0),
+    new TimeSeriesSignal(new DateTime(2018,12,24,16,00,02), 20.0),
+    new TimeSeriesSignal(new DateTime(2018,12,24,16,00,03), 30.0),
+    new TimeSeriesSignal(new DateTime(2018,12,24,16,00,04), 25.0),
+    new TimeSeriesSignal(new DateTime(2018,12,24,16,00,05), 6.0),
+
+    ];
+
+    return [
+      new Series<TimeSeriesSignal,DateTime>(
+        id: _signal.tag,
+        data: exampleData,
+        domainFn: (TimeSeriesSignal sig, _) => sig.time,
+        measureFn: (TimeSeriesSignal sig, _) => sig.engValue)
+    ];
 
   }
 
+}
+
+class TimeSeriesSignal {
+  final DateTime time;
+  final double engValue;
+
+  TimeSeriesSignal(this.time,this.engValue);
 
 }
